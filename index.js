f79c94624665890c03f21a8c2d51ffe2b05125ac
Mohammad-Ali <mohammad@Mohammads-MacBook-Pro.local>
2017-09-28 22:14:32 -0400

    a

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ var dispatcher = function() {
     }
     dispatcher.prototype.sendFile = function(req, res){
         console.log("sending files")
-        //console.log(req.url)
+        console.log(req.url)
         var path = url.parse(req.url).pathname;
-        //console.log(path)
+        console.log(path)
       //  for(var i = 0, listener; i<staticListeners["data"].length; i++) {
             //console.log(staticListeners["data"][i]);
             //console.log(req.url);
